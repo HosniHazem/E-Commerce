9b099aa1faeee53921b99603a5dea10989b1d1f8
HosniHazem <hazem.hosni@aiesec.net>
2022-12-23 07:22:13 +0100

    Swagger ui integration

---
+++ src/main/java/com/succes/ecommerce/SuccessEcommerceAppApplication.java
@@ -0,0 +1,21 @@
+package com.succes.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class SuccessEcommerceAppApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuccessEcommerceAppApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SuccessEcommerceAppApplication.class);
+	}
+
+}
